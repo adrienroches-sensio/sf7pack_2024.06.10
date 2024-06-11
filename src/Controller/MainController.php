58f1ca6f9b58a627a68df8979cd36244f39cf6e6
Adrien Roches <adrien@roc-it.tech>
2024-06-11 12:39:49 +0200

    [Start][Twig] Extend templates from base.html.twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
