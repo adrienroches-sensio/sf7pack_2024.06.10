857f1a88a783c7142a0eb4dc41eabf133ac6fbae
Adrien Roches <adrien@roc-it.tech>
2024-06-13 13:42:50 +0200

    [Master][HttpClient] Implement request

--- src/Event/Search/ApiEventSearch.php
+++ src/Event/Search/ApiEventSearch.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Event\Search;
 
+use Symfony\Component\HttpClient\HttpClient;
+
 final class ApiEventSearch implements EventSearchInterface
 {
     public function __construct(
@@ -13,6 +15,14 @@ final class ApiEventSearch implements EventSearchInterface
 
     public function searchByName(string|null $name = null): array
     {
-        return [];
+        $client = HttpClient::create();
+
+        return $client->request('GET', 'https://www.devevents-api.fr/api/events', [
+            'query' => ['name' => $name],
+            'headers' => [
+                'apikey' => $this->eventsApiKey,
+                'Accept' => 'application/json',
+            ],
+        ])->toArray();
     }
 }
