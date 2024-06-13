85a168f45016d0cdc8d4feeb878d350dd78ecbb7
Adrien Roches <adrien@roc-it.tech>
2024-06-13 10:28:22 +0200

    [Start][DependencyInjection] Inject repository in database search engine

--- src/Event/Search/DatabaseEventSearch.php
+++ src/Event/Search/DatabaseEventSearch.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace App\Event\Search;
 
+use App\Repository\EventRepository;
+
 final class DatabaseEventSearch
 {
+    public function __construct(
+        private readonly EventRepository $eventRepository,
+    ) {
+    }
+
     public function searchByName(string|null $name = null): array
     {
-        return [];
+        if (null === $name) {
+            return $this->eventRepository->list();
+        }
+
+        return $this->eventRepository->searchByName($name);
     }
 }
