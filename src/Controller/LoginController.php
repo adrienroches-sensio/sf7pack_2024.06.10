80cddf78acde6b9435ed443fcb2228a1cd3a0d26
Adrien Roches <adrien@roc-it.tech>
2024-06-14 15:06:04 +0200

    [Master][AdvancedSecurity] Generate api firewall

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class LoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function login(#[CurrentUser] User|null $user): JsonResponse
+    {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json([
+            'user' => $user->getUserIdentifier(),
+            'apikey' => $user->getApiKey(),
+        ]);
+    }
+}
